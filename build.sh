95575771b036fc7d30945a95749318ab58a38bd7
zhongfly <icesshadows@gmail.com>
2021-11-30 18:16:32 +0800

    add wget

--- build.sh
+++ build.sh
@@ -35,7 +35,7 @@ main() {
 
 dependency() {
     sudo apt-get -qq update
-    sudo apt-get -yqq install build-essential checkinstall bison flex gettext git mercurial subversion ninja-build gyp cmake yasm nasm automake pkg-config libtool libtool-bin gcc-multilib g++-multilib clang libgmp-dev libmpfr-dev libmpc-dev libgcrypt-dev gperf ragel texinfo autopoint re2c asciidoc python3-pip docbook2x unzip p7zip-full curl
+    sudo apt-get -yqq install build-essential checkinstall bison flex gettext git mercurial subversion ninja-build gyp cmake yasm nasm automake pkg-config libtool libtool-bin gcc-multilib g++-multilib clang libgmp-dev libmpfr-dev libmpc-dev libgcrypt-dev gperf ragel texinfo autopoint re2c asciidoc python3-pip docbook2x unzip p7zip-full curl wget
     pip3 install rst2pdf mako meson
 }
 
