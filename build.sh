4ac31e4afc4f92264ff908e4c588b60b8b0d05f0
zhongfly <11155705+zhongfly@users.noreply.github.com>
2022-05-04 13:08:01 +0800

    Revert "try build libjxl first if build failed"
    upstream have fixed
    
    This reverts commit 3012bb67f09bffb94745ddea903044ae867d991b.

--- build.sh
+++ build.sh
@@ -42,7 +42,7 @@ build() {
         ninja -C $buildroot/build$bit gcc
     fi
     ninja -C $buildroot/build$bit update
-    ninja -C $buildroot/build$bit mpv || ( ninja -C $buildroot/build$bit libjxl && ninja -C $buildroot/build$bit mpv )
+    ninja -C $buildroot/build$bit mpv
 
     if [ -d $buildroot/build$bit/mpv-$arch* ] ; then
         echo "Successfully compiled $bit-bit. Continue"
